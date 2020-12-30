9bafd8012d05eb7bdb203a9a3ffa0ccb5d18d25c
Marcin Serwin <marcin.serwin2@gmail.com>
2020-12-30 15:48:41 +0100

    Add getJson util function

---
+++ src/utils.js
@@ -0,0 +1,15 @@
+export function getJson(url) {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(JSON.parse(xhr.response));
+            } else {
+                reject(xhr.statusText);
+            }
+        };
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send(null);
+    });
+}
