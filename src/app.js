c26c3243ed63ee48f2f8f8634b98c1703c74fd17
Marcin Serwin <toxyxer@gmail.com>
2020-12-30 15:48:50 +0100

    Display logged account address

--- src/app.js
+++ src/app.js
@@ -1,5 +1,38 @@
+const IDS = {
+    ACCOUNT: "account",
+};
+
 const App = {
-    load() {},
+    account: "",
+
+    load: async () => {
+        await App.loadWeb3();
+        await App.loadAccount();
+        await App.render();
+    },
+
+    async loadWeb3() {
+        App.web3provider = ethereum;
+        window.web3 = new Web3(App.web3provider);
+        try {
+            // Request account access if needed
+            await ethereum.request({ method: "eth_requestAccounts" });
+            // Acccounts now exposed
+            web3.eth.sendTransaction({
+                /* ... */
+            });
+        } catch (error) {
+            // User denied account access...
+        }
+    },
+
+    async loadAccount() {
+        App.account = (await web3.eth.getAccounts())[0];
+    },
+
+    async render() {
+        document.getElementById(IDS.ACCOUNT).textContent = App.account;
+    },
 };
 
 (() => {
