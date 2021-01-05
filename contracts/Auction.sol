30c0465deb51ddbb2a3f900d525dc54adfc4ad86
Marcin Serwin <marcin.serwin2@gmail.com>
2021-01-05 07:24:30 +0100

    Run contracts through autoformatter

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -6,29 +6,34 @@ contract Auction {
     address payable owner;
     string public description;
 
-    uint public startingPrice;
+    uint256 public startingPrice;
 
     uint256 public phaseTwoStart;
     uint256 public phaseThreeStart;
 
     // Mapping to frozen ether
-    mapping(bytes32 => uint) public bids;
+    mapping(bytes32 => uint256) public bids;
 
     struct Bid {
-        uint revealedWeis;
-        uint biddedPrice;
+        uint256 revealedWeis;
+        uint256 biddedPrice;
     }
 
     struct BidReveal {
         address payable bidder;
-        uint biddedPrice;
-        uint salt;
+        uint256 biddedPrice;
+        uint256 salt;
     }
-
 
     mapping(address => Bid) public revealedBids;
 
-    constructor(uint _phaseTwoStart, uint _phaseThreeStart, string memory _description, uint _startingPrice, address payable _owner) {
+    constructor(
+        uint256 _phaseTwoStart,
+        uint256 _phaseThreeStart,
+        string memory _description,
+        uint256 _startingPrice,
+        address payable _owner
+    ) {
         phaseTwoStart = _phaseTwoStart;
         phaseThreeStart = _phaseThreeStart;
         description = _description;
@@ -37,47 +42,63 @@ contract Auction {
     }
 
     modifier onlyInPhaseOne {
-        require(block.timestamp < phaseTwoStart, "This action is only avalible in phase one");
+        require(
+            block.timestamp < phaseTwoStart,
+            "This action is only avalible in phase one"
+        );
         _;
     }
 
     modifier onlyInPhaseTwo {
-        require(block.timestamp >= phaseTwoStart && block.timestamp < phaseThreeStart, 
-            "This action is only avalible in phase two");
+        require(
+            block.timestamp >= phaseTwoStart &&
+                block.timestamp < phaseThreeStart,
+            "This action is only avalible in phase two"
+        );
         _;
     }
 
     modifier onlyInPhaseThree {
-        require(block.timestamp >= phaseThreeStart, "This action is only avalible in phase three");
+        require(
+            block.timestamp >= phaseThreeStart,
+            "This action is only avalible in phase three"
+        );
         _;
     }
 
-    function placeBids(bytes32 bidHash) onlyInPhaseOne public payable {
-        require (msg.value > 0, "Ether provided must be greater than 0");
-        require (bids[bidHash] == 0, "You cannot send the same hash twice");
+    function placeBids(bytes32 bidHash) public payable onlyInPhaseOne {
+        require(msg.value > 0, "Ether provided must be greater than 0");
+        require(bids[bidHash] == 0, "You cannot send the same hash twice");
         bids[bidHash] = msg.value;
     }
 
-    function revealBids(BidReveal[] memory bidReveal) onlyInPhaseTwo public {
-        for (uint i = 0; i < bidReveal.length; i++) {
+    function revealBids(BidReveal[] memory bidReveal) public onlyInPhaseTwo {
+        for (uint256 i = 0; i < bidReveal.length; i++) {
             BidReveal memory bid = bidReveal[i];
 
             require(bid.biddedPrice > 0, "Bidded sum must be greater than 0");
 
             if (revealedBids[bid.bidder].biddedPrice == 0) {
                 revealedBids[bid.bidder].biddedPrice = bid.biddedPrice;
-            }
-            else {
-                require(revealedBids[bid.bidder].biddedPrice == bid.biddedPrice, "Bidded sum cannot be changed");
+            } else {
+                require(
+                    revealedBids[bid.bidder].biddedPrice == bid.biddedPrice,
+                    "Bidded sum cannot be changed"
+                );
             }
 
-            uint weisRelatedToHash = bids[keccak256(abi.encode(bid.bidder, bid.biddedPrice, bid.salt))];
+            uint256 weisRelatedToHash = bids[keccak256(
+                abi.encode(bid.bidder, bid.biddedPrice, bid.salt)
+            )];
 
             revealedBids[bid.bidder].revealedWeis += weisRelatedToHash;
         }
     }
 
-    function withdraw(address payable[] memory withdrawalAddress) onlyInPhaseThree public {
+    function withdraw(address payable[] memory withdrawalAddress)
+        public
+        onlyInPhaseThree
+    {
         // TODO: Implement
     }
 }
--- contracts/AuctionFactory.sol
+++ contracts/AuctionFactory.sol
@@ -1,12 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-import { Auction } from './Auction.sol';
+import {Auction} from "./Auction.sol";
+
 
 contract AuctionFactory {
     address[] public auctions;
 
-    event AuctionCreated(address auctionContract, address owner, uint numAuctions, address[] allAuctions);
+    event AuctionCreated(
+        address auctionContract,
+        address owner,
+        uint256 numAuctions,
+        address[] allAuctions
+    );
 
-    function createAuction(uint256 _phaseTwoStart, uint256 _phaseThreeStart, string memory _description, uint _startingPrice, address payable owner) public { }
+    function createAuction(
+        uint256 _phaseTwoStart,
+        uint256 _phaseThreeStart,
+        string memory _description,
+        uint256 _startingPrice,
+        address payable owner
+    ) public {}
 }
